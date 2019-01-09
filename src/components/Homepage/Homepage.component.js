51fe7aa272f6b9065480c5bed5effebd71f27fa6
Michal <michalplebanski@gmail.com>
2019-01-09 21:03:15 +0100

    adding services section

--- src/components/Homepage/Homepage.component.js
+++ src/components/Homepage/Homepage.component.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import Jumbotron from './Jumbotron/Jumbotron.component';
+import Services from './Services/Services.component';
+
 import './Homepage.scss';
 
 const Homepage = () => {
 	return (
 		<div className='homepage'>
 			<Jumbotron />
+			<Services />
 		</div>
 	);
 }
